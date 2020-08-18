0bc658e904b8d656fb3a23a1e6b3f242c9135177
AgnieteD <agniete.dudaite@gmail.com>
2020-08-18 14:38:51 +0300

    sutvarkyta vidury bomba

--- pirma-pam-08-18/bomba-dideja/index.php
+++ pirma-pam-08-18/bomba-dideja/index.php
@@ -7,17 +7,36 @@
     <title><?php print 'Aš, ' . date('l') . ' ir PHP'; ?></title>
 </head>
 <style>
-    div {
+    .center {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+
+    .center span {
+        color: cyan;
+        font-size: 20px;
+        font-weight: 600;
+    }
+
+    .bg {
         background: url("https://preview.free3d.com/img/2013/10/1863129473415644685/nmhhf19l-900.jpg");
         background-size: contain;
         background-repeat: no-repeat;
-        height: <?php print date('s') * 10; ?>px;
+        height: <?php print date('s') * 10;
+        ?>px;
+        width: <?php print date('s') * 10;
+        ?>px;
     }
 </style>
 
 <body>
-    <div></div>
-    <span><?php print date('s'); ?></span>
+    <div class="center">
+        <div class="bg"></div>
+        <span><?php print date('s'); ?></span>
+    </div>
+
 </body>
 
 </html>
