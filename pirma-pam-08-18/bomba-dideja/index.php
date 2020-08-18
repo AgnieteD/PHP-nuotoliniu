3504d32a76d225c0dce0727c780faf933fa81f36
AgnieteD <agniete.dudaite@gmail.com>
2020-08-18 14:44:53 +0300

    pataisytos klases

--- pirma-pam-08-18/bomba-dideja/index.php
+++ pirma-pam-08-18/bomba-dideja/index.php
@@ -7,20 +7,20 @@
     <title><?php print 'Aš, ' . date('l') . ' ir PHP'; ?></title>
 </head>
 <style>
-    .center {
+    .bomb-container {
         display: flex;
         align-items: center;
         justify-content: center;
         flex-direction: column;
     }
 
-    .center span {
+    .bomb-container span {
         color: cyan;
         font-size: 20px;
         font-weight: 600;
     }
 
-    .bg {
+    .bomb {
         background: url("https://preview.free3d.com/img/2013/10/1863129473415644685/nmhhf19l-900.jpg");
         background-size: contain;
         background-repeat: no-repeat;
@@ -32,8 +32,8 @@
 </style>
 
 <body>
-    <div class="center">
-        <div class="bg"></div>
+    <div class="bomb-container">
+        <div class="bomb"></div>
         <span><?php print date('s'); ?></span>
     </div>
 
