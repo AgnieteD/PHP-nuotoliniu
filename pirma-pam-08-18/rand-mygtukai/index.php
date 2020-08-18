a94b389c0785d94037fdb5498c792edc1820996d
AgnieteD <agniete.dudaite@gmail.com>
2020-08-18 16:27:31 +0300

    random-spalvos-kartojasi

---
+++ pirma-pam-08-18/rand-mygtukai/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Random</title>
+</head>
+<style>
+    .bg-1 {
+        background: red;
+    }
+
+    .bg-2 {
+        background: green;
+    }
+
+    .bg-3 {
+        background: blue;
+    }
+
+    .bg-4 {
+        background: orange;
+    }
+
+    .bg-5 {
+        background: violet;
+    }
+</style>
+
+<body>
+    <button class="bg-<?php print (rand(1, 5)); ?>">BTN1</button>
+    <button class="bg-<?php print (rand(1, 5)); ?>">BTN2</button>
+    <button class="bg-<?php print (rand(1, 5)); ?>">BTN3</button>
+    <button class="bg-<?php print (rand(1, 5)); ?>">BTN4</button>
+    <button class="bg-<?php print (rand(1, 5)); ?>">BTN5</button>
+    
+
+</body>
+
+</html>
+
