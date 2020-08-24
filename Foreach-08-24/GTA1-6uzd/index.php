dbdec1a65f6d22430e830d5f233a1b44d385bc9e
AgnieteD <agniete.dudaite@gmail.com>
2020-08-24 21:26:17 +0300

    gta +- done

--- Foreach-08-24/GTA1-6uzd/index.php
+++ Foreach-08-24/GTA1-6uzd/index.php
@@ -1,76 +1,74 @@
 <?php
-    
-    $game = [
-        'objects' => [
-            [
-                'x' => 50,
-                'y' => 20,
-                'class' => 'car',
-            ],
-            [
-                'x' => 22,
-                'y' => 4,
-                'class' => 'ball',
-            ],
-            [
-                'x' => 30,
-                'y' => 30,
-                'class' => 'cat',
-            ],
-            [
-                'x' => 60,
-                'y' => 0,
-                'class' => 'dog',
-            ],
-        ],
-        'huds' => [
-            [
-                'x' => 0,
-                'y' => 0,
-                'class' => 'huds',
-                'time' => date("h:i"),
-                'bar_1' => rand(1, 100),
-                'bar_2' => rand(1, 100),
-                'weapon' => rand(1, 3),
-                'money' => 1000 + 200,
-                'stars' => 1,
-            ],
-        ]
-    ];
-    
-    foreach ($game['objects'] as $key => $object) {
-//      $x = true;
-//      $y = !$x;
-        
-        $object['on_fire'] = rand(true, false);
-        $object['is_target'] = !$object['on_fire'];
-//      if ($object['on_fire']) {
-//          $object['class'] .= ' fire';
-//      }
-//
-//      if ($object['is_target']) {
-//          $object['class'] .= ' target';
-//      }
-        
-        $object['class'] .= ' ' . ($object['on_fire'] ? 'fire' : 'target');
-//      var_dump($object['on_fire'] ? 'fire' : 'target');
-        $game['objects'][$key] = $object;
-    }
 
-    // $game['hud']['time'] = date("h:i");
-    // $game['hud']['bar_1'] = rand(1, 100);
-    // $game['hud']['bar_2'] = rand(1, 100);
-    // $game['hud']['weapon'] = rand(1, 3) . '.png';
-    
-    //  var_dump($game);
+$game = [
+    'objects' => [
+        [
+            'x' => 50,
+            'y' => 20,
+            'class' => 'car',
+        ],
+        [
+            'x' => 22,
+            'y' => 4,
+            'class' => 'ball',
+        ],
+        [
+            'x' => 30,
+            'y' => 30,
+            'class' => 'cat',
+        ],
+        [
+            'x' => 60,
+            'y' => 0,
+            'class' => 'dog',
+        ],
+    ],
+    'huds' => [
+        [
+            'x' => 0,
+            'y' => 0,
+            'class' => 'huds',
+            'time' => date("h:i"),
+            'bar_1' => rand(1, 100),
+            'bar_2' => rand(1, 100),
+            'weapon' => rand(1, 3),
+        ],
+    ]
+];
+
+foreach ($game['objects'] as $key => $object) {
+    //      $x = true;
+    //      $y = !$x;
+
+    $object['on_fire'] = rand(true, false);
+    $object['is_target'] = !$object['on_fire'];
+    //      if ($object['on_fire']) {
+    //          $object['class'] .= ' fire';
+    //      }
+    //
+    //      if ($object['is_target']) {
+    //          $object['class'] .= ' target';
+    //      }
+
+    $object['class'] .= ' ' . ($object['on_fire'] ? 'fire' : 'target');
+    //      var_dump($object['on_fire'] ? 'fire' : 'target');
+    $game['objects'][$key] = $object;
+
+    $money = 1000 + 200 * ($object['on_fire'] ? count($object) : 1);
+    $game['huds'][0]['money'] = '000' . $money;
+}
+
+//  var_dump($game);
 ?>
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="pricedown bl.ttf">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" />
     <title>gta</title>
     <style>
         body {
@@ -122,36 +120,78 @@
         }
 
         .huds {
-             position: sticky;
-             background: rgba(240, 240, 240, 0.7);
-             width: 150px;
-             height: 400px;
+            position: sticky;
+            background-color: white;
+            width: 200px;
+            height: 250px;
+            box-sizing: border-box;
+            font-family: Pricedown;
+            font-size: 2em;
+            font-weight: 600;
+            border-radius: 10px;
+            text-align: center;
+            padding: 15px;
         }
+
+        .hud {
+            display: flex;
+            margin-bottom: 10px;
+        }
+
+        /* ​ .img {
+            width: 2em;
+            height: 2em;
+        } */
+
+        .right {
+            box-sizing: border-box;
+        }
+
+        .bar {
+            background-color: lightgray;
+            border: 2px solid black;
+        }
+
+        /* ​ .white {
+            background-color: white;
+        } */
+
+        /* ​ .red {
+            background-color: red;
+        } */
     </style>
 </head>
+
 <body>
-<div class="bg">
-    <?php foreach ($game['objects'] as $object): ?>
-        <div class="object <?php print $object['class']; ?>"
-             style="bottom: <?php print $object['y']; ?>%; left: <?php print $object['x']; ?>%;">
-        </div>
-    <?php endforeach; ?>  
+    <div class="bg">
+        <?php foreach ($game['objects'] as $object) : ?>
+            <div class="object <?php print $object['class']; ?>" style="bottom: <?php print $object['y']; ?>%; left: <?php print $object['x']; ?>%;">
+            </div>
+        <?php endforeach; ?>
 
-    <?php foreach ($game['huds'] as $hud): ?>
-        <div class="<?php print $hud['class']; ?>"
-             style="top: <?php print $hud['y']; ?>; left: <?php print $hud['x']; ?>;">
-             <img src="<?php print $hud['weapon']; ?>.png" alt="weapon">
-            <h1><?php print $hud['time']; ?></h1>
+        <div class="huds">
+            <?php foreach ($game['huds'] as $hud) : ?>
+                <div class="hud">
+                    <img style="width: 3em; height: 3em; margin-right: 10px;" src="<?php print $hud['weapon']; ?>.png" alt="weapon">
+                    <div class="right">
+                        <span><?php print $hud['time']; ?></span>
+                        <div class="bar">
+                            <div style="background-color: white; height:20px;width:<?php print $hud['bar_1']; ?>%"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bar">
+                    <div style="background-color: red; height:20px;width:<?php print $hud['bar_2']; ?>%"></div>
+                </div>
+                <span>$<?php print $hud['money']; ?></span>
+                <div>
+                    <?php foreach ($game['objects'] as $object) :
+                        if ($object['on_fire'] ? print '<i class="far fa-star"></i>' : print '<i class="fas fa-star"></i>'); ?>
+                    <?php endforeach; ?>
+                </div>
+            <?php endforeach; ?>
         </div>
-    <?php endforeach; ?>   
-    
-</div>
-</div>
+    </div>
 </body>
+
 </html>
-
-
-
- 
-
-    
