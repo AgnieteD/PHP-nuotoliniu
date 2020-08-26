aa1c29a263b3f00c57c1cadb2d94856d5c640527
AgnieteD <agniete.dudaite@gmail.com>
2020-08-26 13:26:49 +0300

    liko, kad nerefreshintu

---
+++ Form-08-26/3uzd-googly-eyes/index.php
@@ -0,0 +1,27 @@
+<?php
+var_dump($_POST);
+
+$skaicius = intval($_POST['slider'] ?? 25);
+$message = empty($_POST) ? 'Paspausk mygtuką' : $skaicius;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="POST">
+        Dydis: <input name="slider" type="range" min="0" max="50">
+        <button name="submit">Let's do it</button>
+    </form>
+    <p><?= $message; ?></p>
+    <img style="width:<?= $skaicius; ?>em;" src="https://i.ebayimg.com/images/g/lXMAAOSwI4teQQEO/s-l300.jpg">
+</body>
+
+</html>
